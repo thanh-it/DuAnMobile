d8e8f638820cb081928029af2ec262a76cdc59e3
thanh-it <thanhjohn46@gmail.com>
2018-11-05 07:59:25 +0700

    Initial commit

---
+++ app/src/main/java/duan/it/thanh/duanmobile/WelcomeActivity.java
@@ -0,0 +1,26 @@
+package duan.it.thanh.duanmobile;
+
+import android.annotation.SuppressLint;
+import android.support.v7.app.ActionBar;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.os.Handler;
+import android.view.MotionEvent;
+import android.view.View;
+
+import java.util.Timer;
+import java.util.TimerTask;
+
+/**
+ * An example full-screen activity that shows and hides the system UI (i.e.
+ * status bar and navigation/system bar) with user interaction.
+ */
+public class WelcomeActivity extends AppCompatActivity {
+        @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_welcome);
+            Timer timer = new Timer();
+
+    }
+}
