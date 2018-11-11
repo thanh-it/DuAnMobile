f049be7ad452ed83479085ae2989df899c879651
thanh-it <thanhjohn46@gmail.com>
2018-11-11 12:00:32 +0700

    hhh

--- app/src/main/java/duan/it/thanh/duanmobile/Music/ListMusicActivity.java
+++ app/src/main/java/duan/it/thanh/duanmobile/Music/ListMusicActivity.java
@@ -1,15 +1,54 @@
 package duan.it.thanh.duanmobile.Music;
 
+import android.Manifest;
+import android.content.ContentResolver;
+import android.content.Context;
+import android.content.DialogInterface;
+import android.content.pm.PackageManager;
+import android.database.Cursor;
+import android.net.Uri;
+import android.os.Build;
 import android.os.Bundle;
+import android.provider.MediaStore;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
+import android.support.v4.app.ActivityCompat;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.ListView;
+import android.widget.Toast;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 
 import duan.it.thanh.duanmobile.R;
 
 public class ListMusicActivity extends AppCompatActivity {
+    Context context;
+
+    public static final int RUNTIME_PERMISSION_CODE = 7;
+
+    String[] ListElements = new String[] { };
+
+    ListView listView;
+
+    List<String> ListElementsArrayList ;
+
+    ArrayAdapter<String> adapter ;
+
+    ContentResolver contentResolver;
+
+    Cursor cursor;
+
+    Uri uri;
+
+    Button button;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -18,7 +57,100 @@ public class ListMusicActivity extends AppCompatActivity {
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
         toolbar.setNavigationIcon(R.drawable.ic_bakc_24dp);
+        listView = (ListView) findViewById(R.id.listmusicof);
+        context = getApplicationContext();
+        ListElementsArrayList = new ArrayList<>(Arrays.asList(ListElements));
+        adapter = new ArrayAdapter<String>
+                (ListMusicActivity.this, android.R.layout.simple_list_item_1, ListElementsArrayList);
 
+        AndroidRuntimePermission();
+        GetAllMediaMp3Files();
+        listView.setAdapter(adapter);
+
+        listView.setOnItemClickListener(new AdapterView.OnItemClickListener()
+        {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                Toast.makeText(ListMusicActivity.this,parent.getAdapter().getItem(position).toString(), Toast.LENGTH_LONG).show();
+            }
+        });
+    }
+
+    public void GetAllMediaMp3Files(){
+        contentResolver = context.getContentResolver();
+        uri = MediaStore.Audio.Media.EXTERNAL_CONTENT_URI;
+        cursor = contentResolver.query(
+                uri, // Uri
+                null,
+                null,
+                null,
+                null
+        );
+
+        if (cursor == null) {
+            Toast.makeText(ListMusicActivity.this,"Something Went Wrong.", Toast.LENGTH_LONG);
+        } else if (!cursor.moveToFirst()) {
+            Toast.makeText(ListMusicActivity.this,"No Music Found on SD Card.", Toast.LENGTH_LONG);
+        }
+        else {
+            int Title = cursor.getColumnIndex(MediaStore.Audio.Media.TITLE);
+            do {
+                String SongTitle = cursor.getString(Title);
+                ListElementsArrayList.add(SongTitle);
+            } while (cursor.moveToNext());
+        }
+    }
+
+    // Creating Runtime permission function.
+    public void AndroidRuntimePermission(){
+        if(Build.VERSION.SDK_INT>=Build.VERSION_CODES.M){
+            if(checkSelfPermission(Manifest.permission.READ_EXTERNAL_STORAGE)!= PackageManager.PERMISSION_GRANTED){
+                if(shouldShowRequestPermissionRationale(Manifest.permission.READ_EXTERNAL_STORAGE)){
+                    AlertDialog.Builder alert_builder = new AlertDialog.Builder(ListMusicActivity.this);
+                    alert_builder.setMessage("External Storage Permission is Required.");
+                    alert_builder.setTitle("Please Grant Permission.");
+                    alert_builder.setPositiveButton("OK", new DialogInterface.OnClickListener() {
+                        @Override
+                        public void onClick(DialogInterface dialogInterface, int i) {
+                            ActivityCompat.requestPermissions(
+                                    ListMusicActivity.this,
+                                    new String[]{Manifest.permission.READ_EXTERNAL_STORAGE},
+                                    RUNTIME_PERMISSION_CODE
+                            );
+                        }
+                    });
+                    alert_builder.setNeutralButton("Cancel",null);
+                    AlertDialog dialog = alert_builder.create();
+                    dialog.show();
+                }
+                else {
+                    ActivityCompat.requestPermissions(
+                            ListMusicActivity.this,
+                            new String[]{Manifest.permission.READ_EXTERNAL_STORAGE},
+                            RUNTIME_PERMISSION_CODE
+                    );
+                }
+            }else {
+
+            }
+        }
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, String[] permissions, int[] grantResults){
+
+        switch(requestCode){
+
+            case RUNTIME_PERMISSION_CODE:{
+
+                if(grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED){
+
+                }
+                else {
+
+                }
+            }
+        }
     }
 
 }
